90fb3bb5bdadd09e0421c38edb764995d5ec0139
Aidan Lee <12875732@student.uts.edu.au>
2021-09-27 11:15:23 +1000

    added button click and created random number

--- src/Screens/PVTScreen/SimpleCircleButton.js
+++ src/Screens/PVTScreen/SimpleCircleButton.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import { View, StyleSheet, TouchableOpacity, Text, Dimensions } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text, Dimensions, Alert} from 'react-native';
 
-export default class SimpleCircleButton extends React.Component {
+export default class SimpleCircleButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { timer: 3};
+    this.state = { colorId:0 };
+  }
+  onPress = () => {
+    if (this.state.colorId === 1)
+    {
+      this.setState({colorId: 0});
+    }
+    else 
+    {
+      this.setState({colorId: 1});
+      const min = 1;
+      const max = 4;
+      const rand = min + Math.random() * (max - min);
+      alert(rand);
+    }
+  };
+
   render(){
-    let localStyles = styles(this.props) //need to load styles with props because the styles rely on prop values
-
     return (
-      <View style={localStyles.container}>
+      <View style={styles.container}>
         <TouchableOpacity
           activeOpacity={.8} //The opacity of the button when it is pressed
-          style = {localStyles.button}
-          onPress = {this.props.onPress}          
+          style={this.state.colorId === 1? styles.buttonOff : styles.buttonOn}
+          onPress={()=>this.onPress()}       
         >
-            <Text style={localStyles.text}>CLICK TO START</Text>
+            <Text style={styles.text}>CLICK TO START</Text>
           {this.props.children}
         </TouchableOpacity>
       </View>
@@ -21,10 +39,11 @@ export default class SimpleCircleButton extends React.Component {
 }
 
 
+
 const devHeight = Dimensions.get('window').height;
 const devWidth = Dimensions.get('window').width;
 
-const styles = (props) => StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     position: 'relative',
     zIndex: 0,
@@ -32,7 +51,7 @@ const styles = (props) => StyleSheet.create({
     alignItems: 'center',
     marginBottom:50
   },
-  button: {
+  buttonOn: {
     backgroundColor: 'rgba(0,210,0,1)',
     justifyContent: 'center',
     alignContent: 'center',
@@ -41,13 +60,22 @@ const styles = (props) => StyleSheet.create({
     width: devWidth * 4 / 5,
     height: devWidth * 4 / 5,
     borderColor: 'rgba(0,210,0,1)'
-  }
-  ,
+  }  ,
+  buttonOff: {
+    backgroundColor: 'rgba(210,0,0,1)',
+    justifyContent: 'center',
+    alignContent: 'center',
+    borderWidth: 3,
+    borderRadius: devWidth * 4 / 5,
+    width: devWidth * 4 / 5,
+    height: devWidth * 4 / 5,
+    borderColor: 'rgba(210,0,0,1)'
+  }  ,
   text: {
     justifyContent: 'center',
     alignContent: 'center',
     marginLeft: devWidth /4.2
-  }
+  },
 });
 
 /*NOTE: THIS IS USED BASED ON A TUTORIAL AT https://www.jsparling.com/round-buttons-in-react-native/*/
